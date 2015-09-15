d940b7f374ea52b1d4030908933ecd0a43ea8479
Vgr E. Barry <vgr255@live.ca>
2015-09-15 11:21:44 -0400

    Add Fanny's twin's info

---
+++ Characters/_data/unknown.py
@@ -0,0 +1,27 @@
+FILE = "Unknown"
+
+NAME = "???"
+
+CLASS = "Metamorph"
+
+WEAPON = None
+
+BIRTH = "September 22nd, 1886"
+
+BIRTH_LOCATION = "Void"
+
+SUMANSIAN = True
+
+SUMANSIAN_TWIN = "Fanny"
+
+SUMMARY = ""
+
+ABILITIES = ""
+
+BACKSTORY = ""
+
+HIGHLIGHTS = ""
+
+FAMILY = (("Isa", "Sumansian sister"), ("Zoe", "Sumansian sister"))
+
+SUMMONS = ()
