b3ce5bbfb44e1bb6dc25bb40fda12a91ccfa21ff
Vgr E. Barry <vgr255@live.ca>
2015-09-17 11:27:39 -0400

    Add more information for Liakin

--- Characters/_data/liakin.py
+++ Characters/_data/liakin.py
@@ -22,11 +22,11 @@ SUMANSIAN = True
 
 SUMANSIAN_TWIN = "Gerald Kennedy"
 
-SUMMARY = "Liakin can teleport herself and a few more people to a location that she already visited before, saw somewhere, or know exactly where it is (as to not end up in a wall). She wields two Mythril daggers, the Blue Bolt, which she can use to attack people, by teleporting."
+SUMMARY = "Liakin can teleport herself and a few more people to a location that she already visited before, saw somewhere, or know exactly where it is (as to not end up in a wall). She wields two Mythril daggers, called the Blue Bolt, which she can use to attack people, by teleporting. She is also quite strong, despite being short. She is the vice-president of the International Multi-Agency Council for Tracking and Interception of National and International Threats, abbreviated as IM-ACTION-IT, which is a program between various agencies to cooperate and share their efforts. She is also the lead of the LIAKIN program, the Leak and Information Analysing, and Killing of Internal Nuisances, taking care of enemy espionage and ensuring the private data remains private."
 
 ABILITIES = "Her fast speed and teleporting ability means she can easily dispatch a whole group of enemies. She's never static, teleporting back and forth between the front and back lines. She often aids Marisa by teleporting her to the wounded, or the other way around. She's also able to communicate with other people through telepathy."
 
-BACKSTORY = "She used to be nothing more than a simple civilian, but the ICS had decided to track and protect her, as they determined she had some potential. When some of Ghi's men decide to attack her and Marisa, Jeremy Alexander goes to save them. Then, to protect them further, he trained Liakin, and she became much stronger. She later joined <i>them</i>."
+BACKSTORY = "She used to be nothing more than a simple civilian, but the ICS had decided to track and protect her, as they determined she had some potential. When some of Ghi's men decide to attack her and Marisa, Jeremy Alexander goes to save them. Then, to protect them further, he trained her, and she became much stronger. To protect her from eventual future assaults, he gave her the codename of Liakin, which was later used to establish the fake name of Anabella Liakin. Her real name is still unknown to many. She later joined <i>them</i> on demand from Quan and Zoe, thus completing the team."
 
 HIGHLIGHTS = "During most of the large-scale battles the S-Team undertook, she was always there, teleporting back and forth between the battlefield and the ICS, where Marisa would heal the wounded."
 
