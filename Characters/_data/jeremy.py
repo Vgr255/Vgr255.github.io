f5aea5702163181d17278c6a1e919b32ef63a479
Vgr E. Barry <vgr255@live.ca>
2015-09-29 10:37:08 -0400

    Add more data to Jeremy

--- Characters/_data/jeremy.py
+++ Characters/_data/jeremy.py
@@ -22,13 +22,13 @@ SUMANSIAN = True
 
 SUMANSIAN_TWIN = "Amelia Bater"
 
-SUMMARY = "Recruited by Victor Morika back when he was a taxi driver in downtown Manhattan, Jeremy was a founding member of the current ICS. He is a strong-willed fighter who puts the lives of his comrades before anything else, even his own. He is often seen on the front lines of any large-scale battle."
+SUMMARY = "Recruited by Victor Morika back when he was a taxi driver in downtown Manhattan, Jeremy was a founding member of the current ICS. He is a strong-willed fighter who puts the lives of his comrades before anything else, even his own. He is often seen on the front lines of any large-scale battle. He can control fire, and wields the Elketsir. He has a strong sense of leadership and is most often the leader on a mission, and can always be seen in any large-scale mission."
 
-ABILITIES = "Unlike what could be thought, nobody can summon the Phoenix, as he is the Phoenix. He can morph into the Phoenix and can conceal himself as a white eagle. He can travel through the means of lines of fire he makes. He wields a powerful, magical sword."
+ABILITIES = "Unlike what could be thought, nobody can summon the Phoenix, as he is the Phoenix. He can morph into a large white eagle, which can turn into the Phoenix. He can travel through the means of lines of fire he makes, and he is also able to split himself in particles of fire to avoid damage, then reunite to keep fighting. He wields a powerful, magical sword called the Elketsir."
 
-BACKSTORY = "He was living in Manhattan with his girlfriend Emily as a taxi driver when Victor Morika came to recruit him, in 1999. He managed to convince him to come, and he got in the CIA. Not much is known about him prior to his recruitment in the CIA, besides that Jeremy Alexander isn’t his real name. Presumably it starts by M, given his letter as part of them."
+BACKSTORY = "He was living in Manhattan with his girlfriend Emily as a taxi driver when Victor Morika came to recruit him, in 1999. He managed to convince him to come, and he got in the CIA. Not much is known about him prior to his recruitment in the CIA, besides that Jeremy Alexander isn’t his real name. Presumably it starts by M, given his letter as part of <i>them</i>. He started the ISAUCC program while secretly training Marisa, to benefit the most people from his training."
 
-HIGHLIGHTS = "During the battle against Ghi's forces in 2026, he single-handedly defeated most of them, and helped Marisa create a wall to protect the ICS' forces while Katelyn and Ghi clashed. During The Great Battle in 2034, he managed to hold back most of the enemies and gained time while Simon and his squad were moving in position. He also led them to help Arya after Gerald sought their help."
+HIGHLIGHTS = "During the battle against Ghi's forces in 2026, he single-handedly defeated most of them, and helped Marisa create a wall to protect the ICS' forces while Katelyn and Ghi clashed. During The Great Battle in 2034, he managed to hold back most of the enemies and gained time while Simon and his squad were moving in position. He also led <i>them</i> to help Arya after Gerald sought their help."
 
 FAMILY = (("Amelia Bater", "Sumansian twin / Girlfriend"), ("Arya Alexander", "Daughter"))
 
