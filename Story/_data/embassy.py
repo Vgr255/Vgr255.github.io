39d05bf2c6f1eea61ae8cc2e8788526a019c3471
Vgr E. Barry <vgr255@live.ca>
2015-10-06 19:16:48 -0400

    Add the "Passage at the Embassy" story
    Adapted from a dream I had, and made into Evelyn's past to keep
    continuity with the rest. Evelyn's past had never been established
    before, so it is safe to do so now.

---
+++ Story/_data/embassy.py
@@ -0,0 +1,66 @@
+# That one came in a dream on October 6th, 2015. Adapted for the narrative and made into Evelyn's past
+
+TITLE = "Passage at the Embassy (Evelyn's origins)"
+
+DATA = """
+
+Set: Near an unknown embassy, in the parking lot. Victor Morika is fighting with two people, a couple of a man and a woman. \
+Surrounding the embassy is a seemingly-endless and tall forest. The year is 1980.
+
+Morika had just knocked the man out on the ground. Quickly, he runs towards the couple's daughter, who had been \
+watching aside for the whole time, grabs her arm, and proceeds to run towards the embassy.
+
+The girl has her hair down her back tied in a uniform braid. She is young, and does not resist when Victor brings her towards the embassy.
+
+"Don't let them in!", he shouts at the guards just outside the embassy. Having stepped in the protection field, he can no longer make use \
+of his magical abilities and has to rely on technology to defend himself.
+
+He shouts warnings in the main hall where people are talking, and makes quick way to the staircase. \
+Knowing that his time is short, he quickly runs up the stairs, closely followed by the girl.
+
+At the top awaits Brandon, with a something that looks like an odd mix between a backpack and a harness. \
+He gives it to Victor, who quickly puts it on his back. He pulls the girl forward, pressing her against his chest, \
+and binds her with some straps. She's weak at this point, and seems to be about to pass out.
+
+Footsteps are heard coming from the staircase. Quickly, Brandon, runs behind the stairwell to hide, just as the \
+couple emerges from the stairs. Victor stands on the ledge, facing them with their daughter attached to him. During \
+the split second that they hesitate to move, Morika falls backwards into space.
+
+They rush to try to catch him, but he's already falling at a high speed down the tall building, almost reaching the \
+trees' peaks. Halfway through, he deploys two large wings from his backpack and flies into the horizon, out of reach. \
+Behind them, Brandon silently makes his way down the stairs.
+
+***
+
+"Victor, she's awake," says the man sitting next to her. She sits up, already feeling much better.
+
+The man called Victor approached. It was the man who got her away from her parents at the embassy. \
+She didn't know if she was happy or sad about that. It seemed like a good idea when her parents were \
+down on the ground, but when she entered the embassy's vicinity, she started to weaken.
+
+"You didn't realize it, right?", the first man said, "You survive thanks to magic."
+
+"But... why did... I felt dead when entering that place?"
+
+"It's because that embassy has a special protection field, and blocks all magic. It also permanently \
+blocked your parents' abilities as long as you don't grant them power again. They were neutralized and \
+are being detained as we speak."
+
+"So, I... am free?", she asks with hesitation. "If you want. We're not going to hold you back, but we would \
+really like to have you on our team," Victor replies.
+
+A woman appears, and looks at her. "She'll do," she says simply, turns around and walks back out without \
+any further word.
+
+"Who..." she begins, but is promptly interrupted by the first man, "She's Zoe, our leader. Yours too, \
+if you so choose."
+
+"If I join... Will that make me stronger? To defend myself?"
+
+"Yes. We'll train you and make the best of your abilities," Victor says.
+
+"Then... I'll join... I'm Evelyn," she concludes after some thinking.
+
+"I expected as much. I'm Victor, and this man here is Luke. Welcome aboard."
+
+"""
