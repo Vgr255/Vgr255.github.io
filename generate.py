3d213a21502413c628fdfc0df69ea844a52d5a46
Vgr E. Barry <vgr255@live.ca>
2015-10-06 19:06:25 -0400

    Tweak to the main index

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-# tool to automatically generate a file with the character's content
+# tool to automatically generate all of the files
 
 import collections
 import importlib
@@ -291,7 +291,7 @@ def generate_index():
                     data.append(os.path.join(folder, file))
 
         for folder in data:
-            f.write("{0}{0}<h1>{1}</h1>\n".format(TAB, CHAR_REF.format("/".join((folder[len(os.getcwd())+1:].replace(os.sep, "/"), "index")), os.path.split(folder)[1])))
+            f.write("{0}{0}<h1>{1}</h1>\n".format(TAB, CHAR_REF.format("/".join((folder[len(os.getcwd())+1:].replace(os.sep, "/"), "index")), os.path.split(folder)[1] + " Index")))
 
         f.write("\n{0}{0}<p><a href=\"{1}\" title=\"{2}\">{2}</a></p>\n{0}</body>\n</html>\n".format(TAB, LINK, "Source Code on GitHub"))
 
