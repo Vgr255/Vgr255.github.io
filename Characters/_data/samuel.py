0ebe5efc3470f87716ca99972ca1e414f32d2af4
Vgr E. Barry <vgr255@live.ca>
2015-10-07 09:03:05 -0400

    Add Samuel

---
+++ Characters/_data/samuel.py
@@ -0,0 +1,23 @@
+FILE = "Samuel"
+
+NAME = "Samuel"
+
+CLASS = "Agent"
+
+WEAPON = ""
+
+BIRTH = ""
+
+BIRTH_LOCATION = ""
+
+SUMMARY = ""
+
+ABILITIES = ""
+
+BACKSTORY = ""
+
+HIGHLIGHTS = ""
+
+FAMILY = ()
+
+SUMMONS = ()
