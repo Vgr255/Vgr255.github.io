a2eb001ac9df996096bd9a6fa7285e87a87d82a6
Vgr E. Barry <vgr255@live.ca>
2015-10-07 09:22:23 -0400

    Add Zoe to the Embassy story

--- Story/_data/embassy.py
+++ Story/_data/embassy.py
@@ -2,7 +2,7 @@
 
 TITLE = "Passage at the Embassy (Evelyn's origins)"
 
-CHARACTERS = ("Evelyn", "Brandon", "Victor Morika", "Luke Walker")
+CHARACTERS = ("Evelyn", "Brandon", "Victor Morika", "Luke Walker", "Zoe")
 
 DATA = """
 
