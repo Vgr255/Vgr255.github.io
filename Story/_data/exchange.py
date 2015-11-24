271b49e56e4bbb1d8d9eff0861e0dea1979d7f74
Vgr E. Barry <vgr255@live.ca>
2015-11-24 15:47:07 -0500

    Add the data exchange story

---
+++ Story/_data/exchange.py
@@ -0,0 +1,45 @@
+# Had this one not long after giving Xu a more defined background
+# decided to make her more active instead of just a background character
+
+TITLE = "Exchanging data and Xu's first appearance"
+
+CHARACTERS = ("Luke Walker", "Phaleën", "Xu")
+
+DATA = """
+
+Set: In a crowded place in Moscow, near a small cafe, surrounded by skyscrapers. The year is 2014.
+
+Phaleën is sitting at a table, and had just ordered a coffee for himself. He has a laptop case next to \
+him and is waiting for someone. A few kilometers away, Luke Walker is sitting in front of a computer in \
+a small hideout.
+
+After a few minutes, a man sits in front of Phaleën and hands him a USB key. He pulls out a used laptop \
+from the case, and plugs in the key. He looks and skims through the files. After a few minutes, he pulls \
+out the key, puts it in his pocket and sets the laptop back in its case.
+
+"It's clear. These are the files we needed," he says. Moments later, a young chinese girl comes around the \
+corner carrying a suitcase. She comes to their table, bows to Phaleën, and puts the suitcase on the table. \
+Phaleën turns a few knobs, and the suitcase pops open.
+
+He turns it around, and the man looks into it carefully. He nods and closes the suitcase. Phaleën stands up, \
+nods at the man, and then heads back with the girl, holding her arm as if he wanted to make sure she didn't escape.
+
+A gunshot is heard, and Phaleën turns around just as a long 50mm bullet hits his head, bouncing off the \
+protective shield he erected around them. Phaleën releases his grip on the girl's arm, who instantly turns \
+around. She pulls two guns out of nowhere, aims at the sniper who shot at them, and shoots magical projectiles \
+out of her guns.
+
+Through a hand wave, she makes her projectiles explode atop a skyscraper, possibly killing off the sniper. \
+Phaleën looks at the man now running through the commotion, then turns around and walks off.
+
+The chinese attendee runs up to him, "Lane, aren't you going to go off after him?", she asks. He pulls out a \
+set of keys from his pocket, and unlocks the car close to them. "We got what we needed, it doesn't matter at \
+this point," he replies. "But," she begins, promptly interrupted by him, "Listen, Xu, sometimes you need to \
+stop trying to make justice. We got what we needed, the rest doesn't matter. Let's just move on, Luke's \
+waiting on us.
+
+She sighs, and gets in the car along him. She'd have loved to just kill that man and get back what was in \
+the suitcase - what was it anyway? It wasn't money, but what could it be? She tried to forget about it, and \
+instead focus on what they needed to do next.
+
+"""
