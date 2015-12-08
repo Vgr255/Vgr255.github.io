2e57089e5bac20a58b1b5d934cba7b1a0be0361e
Vgr E. Barry <vgr255@live.ca>
2015-12-08 11:24:47 -0500

    First draft of the "Destruction" story

---
+++ Story/_data/destruction.py
@@ -0,0 +1,55 @@
+# had this one for a very long time, but only wrote it down now
+
+TITLE = "Destruction of the ICS and retreat into the Void"
+
+CHARACTERS = ("Diana", "Jordan", "Jeremy Alexander", "Noémye", "Quan", "Rachel")
+
+DATETIME = "2042"
+
+DATA = """
+
+"30 seconds to contact," a man says, his voice echoing a few kilometres away in the ICS' \
+control center. Rachel is still at her terminal, monitoring the activities, and a man is \
+standing in the middle, looking at the satellite plans and street cameras. He's about to \
+finally wrap this case, which had been lingering for years upon years.
+
+Walking at a slow but steady pace, the man who spoke is about to reach his target. He's \
+almost within range, but then he stops and turns around, looking at the ICS building.
+
+The one who seems to be the operation leader looks at the screen, and says "What's wrong? \
+Get him, now!", but the agent replies with "Rachel... A portal to the Void is being opened... \
+And I'm not the one doing it"
+
+Rachel, back at the command center, switches some screens and looks at a satellite view of \
+the city, only to see a portal effectively being opened hundreds of metres above the ICS.
+
+By then, everyone in town had turned to look at this new sight, and the agent is running \
+towards the ICS. "Rachel!", he shouts, "Evacuate the ICS! This is stronger than anything \
+we've fought before!"
+
+Immediately, she triggers the evacuation protocol, and orders everyone to leave the \
+building, witnessed by the obviously displeased operation leader, but he doesn't try \
+to stop her.
+
+"Jordan," she says, "Open a portal for Quan, and leave it open."
+
+But he didn't need her orders to do so. He was already in the process of opening a \
+portal, and almost as soon as he was done, a young boy comes out of it. Jordan looks \
+at him, and quickly says "Come on Quan, get to the ICS, Rachel's waiting for you."
+
+The young boy named Quan runs through the cars and quickly to the ICS, while Jordan \
+makes his way through the portal.
+
+***
+
+Just before it hits her, Diana manages to catch the projectiles hurled at her and uses \
+them as her own weapon. She surfs around some enemies and throws back the projectiles.
+
+Just at her side, Alexander does a backflip and deviates an attack using his sword, then \
+generates a large energy line that goes to slash against his assailants.
+
+A giant bird-like animal comes hurling at Alexander while he's counter-attacking, but is \
+quickly struck down by Noémye, with her spear. She then jumps off it and towards another \
+volatile.
+
+"""
